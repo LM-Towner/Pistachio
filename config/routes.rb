dd1aba8555cc12691cd59c68729d75b81ded45a5
La-Keisha Towner <ltowner622@gmail.com>
2017-05-07 00:56:56 -0400

    changed the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   #define root URL
   root 'pages#index'
 
-  get 'home' => 'pages#home'
+  get '/home' => 'pages#home'
 
-  get 'profile' => 'pages#profile'
+  get '/profile' => 'pages#profile'
 
-  get 'explore' => 'pages#explore'
+  get '/explore' => 'pages#explore'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
